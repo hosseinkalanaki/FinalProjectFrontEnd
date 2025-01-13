bad1ac1f28b2ea332c6621bc00de18bc53ea04da
Roozbehyo <rz.yousefi.work@gmail.com>
2025-01-13 15:56:12 +0330

    initial design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+// @ts-ignore
 import { Component } from '@angular/core';
 
+// @ts-ignore
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyApp';
+   centers = [
+    { name: 'مرکز شماره 1', temp1: 20, temp2: 26, hum1: 30 },
+    { name: 'مرکز شماره 2', temp1: 22, temp2: 28, hum1: 35 },
+    { name: 'مرکز شماره 3', temp1: 21, temp2: 27, hum1: 33 }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+// @ts-ignore
 import { NgModule } from '@angular/core';
+// @ts-ignore
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent
